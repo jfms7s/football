96b32627d0ba5e88ff566486ddd6c08ad0113335
João Silva <jfms7s@gmail.com>
2016-09-16 16:22:59 +0100

    gitignore

--- .tmp/appValidoo/app.js
+++ .tmp/appValidoo/app.js
@@ -9,7 +9,7 @@
 
 (function () {
     angular.module('app').config(['$httpProvider', '$locationProvider', 'ConfigsProvider', function ($httpProvider, $locationProvider, ConfigsProvider) {
-        //$locationProvider.html5Mode(true);
+        $locationProvider.html5Mode(true);
         $httpProvider.defaults.headers.get = { 'X-Auth-Token': ConfigsProvider.getApiKey() };
     }]);
 })();
